c9020ab5d74838ae8e414967205cd178088a3c0a
hoiheart <hoiheart@gmail.com>
2020-01-03 16:30:14 +0900

    feat: 플레이어 크롤링 삭제

--- scripts/teams.js
+++ scripts/teams.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  console.log('teams & players loading...');
+  console.log('teams loading...');
 
   try {
     const browser = await puppeteer.launch();
@@ -33,23 +33,11 @@ const fs = require('fs');
 
       await fs.mkdirSync('./data/rosters/', { recursive: true });
       await fs.writeFileSync(`./data/rosters/${teamID}.json`, JSON.stringify(rostersJson));
-
-      const players = [...rostersJson.athletes];
-
-      for (let player of players) {
-        const playerID = player.id;
-        await page.goto(`http://www.espnfc.com/player/${playerID}?xhr=1`);
-        await page.content(); 
-        const playerJson = await page.evaluate(() => JSON.parse(document.querySelector("body").innerText));
-        
-        await fs.mkdirSync('./data/players/', { recursive: true });
-        await fs.writeFileSync(`./data/players/${playerID}.json`, JSON.stringify(playerJson));
-      }
     }
     
     await browser.close();
 
-    console.log('teams & players loaded success');
+    console.log('teams loaded success');
   } catch(e) {
     console.log(e);
   }
